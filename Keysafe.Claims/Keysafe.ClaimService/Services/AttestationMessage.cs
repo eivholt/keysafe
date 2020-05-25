76fe09ec6d3df18bfbdaf8e070a37b7e4ef9808c
Eivind Holt <eivholt@gmail.com>
2020-05-25 22:28:54 +0200

    Claims service

---
+++ Keysafe.Claims/Keysafe.ClaimService/Services/AttestationMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Keysafe.ClaimService.Services
+{
+    public class AttestationMessage
+    {
+        public string AttestUuid { get; set; }
+        public Google.Protobuf.WellKnownTypes.Timestamp ValidFrom { get; set; }
+        public Google.Protobuf.WellKnownTypes.Timestamp ValidTo { get; set; }
+        User user = 4;
+        Organization organization = 5;
+        Lock lock = 6;
+    }
+}
--- Keysafe.Claims/Keysafe.ClaimService/Services/ClaimsService.cs
+++ Keysafe.Claims/Keysafe.ClaimService/Services/ClaimsService.cs
@@ -4,25 +4,65 @@ using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using RestSharp;
+using Tangle.Net.Cryptography;
+using Tangle.Net.Entity;
+using Tangle.Net.Mam.Entity;
+using Tangle.Net.Mam.Merkle;
+using Tangle.Net.Mam.Services;
+using Tangle.Net.Repository;
 
 namespace Keysafe.ClaimService
 {
     public class ClaimsService : Claims.ClaimsBase
     {
         private readonly ILogger<ClaimsService> m_logger;
+        private static readonly Seed s_sampleLock1Seed = new Seed("W9XOWBFMSZKXNLHZWDTPUQTW99OSPFHSLEGAQMR9FDSLYUPGCMZBGCEXCN9BBW9CBNHNQACFHYJEQOU99");
+        private static readonly string s_sampleLock1SideKey = "E&AVejgjEk36k@-%";
+
         public ClaimsService(ILogger<ClaimsService> logger)
         {
             m_logger = logger;
         }
 
-        public override Task<VerifiableClaimsCreateReply> CreateVerifiableClaims(VerifiableClaimsCreateRequest request, ServerCallContext context)
+        public override async Task<VerifiableClaimsCreateReply> CreateVerifiableClaims(VerifiableClaimsCreateRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new VerifiableClaimsCreateReply
+            // Connect to a node on one of the main networks.
+            var repository = new RestIotaRepository(new RestClient("https://nodes.devnet.thetangle.org:443"));
+            var factory = new MamChannelFactory(CurlMamFactory.Default, CurlMerkleTreeFactory.Default, repository);
+            var channel = factory.Create(Mode.Restricted, s_sampleLock1Seed, SecurityLevel.Medium, s_sampleLock1SideKey);
+
+            var attestation = CreateAttestationFromRequest(request);
+            var message = channel.CreateMessage(TryteString.FromAsciiString(request.));
+
+            try
             {
-                Success = false,
-                Message = "Not implemented.",
-                Attesthash = "N/A"
-            });
+                await channel.PublishAsync(message);
+                m_logger.LogInformation("CreateVerifiableClaims, remove before production!", message );
+
+                return new VerifiableClaimsCreateReply
+                {
+                    Success = false,
+                    Message = "Not implemented.",
+                    AttestHash = "N/A"
+                };
+            }
+            catch (Exception ex)
+            {
+                m_logger.LogError("CreateVerifiableClaims", ex);
+
+                return new VerifiableClaimsCreateReply
+                {
+                    Success = false,
+                    Message = ex.Message + ex.StackTrace,
+                    AttestHash = "error"
+                };
+            }
+        }
+
+        private string CreateAttestationFromRequest(VerifiableClaimsCreateRequest request)
+        {
+            throw new NotImplementedException();
         }
     }
 }
